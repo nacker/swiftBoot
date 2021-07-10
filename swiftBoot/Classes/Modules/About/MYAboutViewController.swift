b8249cd260c3695b29a36a1687b699a9e34ef9ff
nacker <nacker@163.com>
2021-07-10 18:26:18 +0800

    基础框架

---
+++ swiftBoot/Classes/Modules/About/MYAboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MYAboutViewController.swift
+//  swiftBoot
+//
+//  Created by nacker on 2021/7/10.
+//
+
+import UIKit
+
+class MYAboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
