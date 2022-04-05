686e48719706a2a49c3ee9450442a47398b56ead
nacker <nacker@163.com>
2022-04-05 21:39:36 +0800

    lib

---
+++ swiftBoot/Classes/Other/Lib/TimedSilver/Sources/UIKit/UIImage+TSOrientation.swift
@@ -0,0 +1,90 @@
+//
+//  UIImage+Orientation.swift
+//  TimedSilver
+//  Source: https://github.com/hilen/TimedSilver
+//
+//  Created by Hilen on 2/17/16.
+//  Copyright © 2016 Hilen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+import CoreGraphics
+import Accelerate
+
+//https://github.com/cosnovae/fixUIImageOrientation/blob/master/fixImageOrientation.swift
+
+public extension UIImage {
+    /**
+     Fix the image's orientation
+     
+     - parameter src: the source image
+     
+     - returns: new image
+     */
+    class func ts_fixImageOrientation(_ src:UIImage) -> UIImage {
+        
+        if src.imageOrientation == UIImage.Orientation.up {
+            return src
+        }
+        
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        
+        switch src.imageOrientation {
+        case UIImage.Orientation.down, UIImage.Orientation.downMirrored:
+            transform = transform.translatedBy(x: src.size.width, y: src.size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+            break
+        case UIImage.Orientation.left, UIImage.Orientation.leftMirrored:
+            transform = transform.translatedBy(x: src.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+            break
+        case UIImage.Orientation.right, UIImage.Orientation.rightMirrored:
+            transform = transform.translatedBy(x: 0, y: src.size.height)
+            transform = transform.rotated(by: CGFloat(-Double.pi))
+            break
+        case UIImage.Orientation.up, UIImage.Orientation.upMirrored:
+            break
+        @unknown default:
+            break
+        }
+        
+        switch src.imageOrientation {
+        case UIImage.Orientation.upMirrored, UIImage.Orientation.downMirrored:
+            transform.translatedBy(x: src.size.width, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            break
+        case UIImage.Orientation.leftMirrored, UIImage.Orientation.rightMirrored:
+            transform.translatedBy(x: src.size.height, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+        case UIImage.Orientation.up, UIImage.Orientation.down, UIImage.Orientation.left, UIImage.Orientation.right:
+            break
+        @unknown default:
+            break
+        }
+        
+        let ctx:CGContext = CGContext(data: nil, width: Int(src.size.width), height: Int(src.size.height), bitsPerComponent: src.cgImage!.bitsPerComponent, bytesPerRow: 0, space: src.cgImage!.colorSpace!, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
+        
+        ctx.concatenate(transform)
+        
+        switch src.imageOrientation {
+        case UIImage.Orientation.left, UIImage.Orientation.leftMirrored, UIImage.Orientation.right, UIImage.Orientation.rightMirrored:
+            ctx.draw(src.cgImage!, in: CGRect(x: 0, y: 0, width: src.size.height, height: src.size.width))
+            break
+        default:
+            ctx.draw(src.cgImage!, in: CGRect(x: 0, y: 0, width: src.size.width, height: src.size.height))
+            break
+        }
+        
+        let cgimage:CGImage = ctx.makeImage()!
+        let image:UIImage = UIImage(cgImage: cgimage)
+        
+        return image
+    }
+}
+
+
+
+
+
